75f10e5e19643265156c55f9b01bec484335133e
NDJafu <giaphu432@gmail.com>
2023-11-20 22:36:58 +0700

    added GsonRequest for api calls

--- app/src/main/java/com/weatherwise/MainActivity.java
+++ app/src/main/java/com/weatherwise/MainActivity.java
@@ -2,7 +2,7 @@ package com.weatherwise;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,39 +11,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 import org.jetbrains.annotations.NotNull;
 
+
 public class MainActivity extends AppCompatActivity {
-    TextView longitude, latitude;
+    TextView longitudeTextView, latitudeTextView, apiKey;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    final String CURRENT_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
+    final String FORECAST_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?";
+    final String OPENWEATHER_API_KEY = "1b7ce001fbf4143a1618c866d8204c56";
+    final String LAT_PARAM = "lat";
+    final String LONG_PARAM = "lon";
+    final String APIKEY_PARAM = "appid";
+
+    Uri builtUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        longitude = findViewById(R.id.textViewLongitude);
-        latitude = findViewById(R.id.textViewLatitude);
+        longitudeTextView = findViewById(R.id.textViewLongitude);
+        latitudeTextView = findViewById(R.id.textViewLatitude);
+        apiKey = findViewById(R.id.textViewAPI);
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
+
+        getWeather(CURRENT_WEATHER_BASE_URL, OPENWEATHER_API_KEY);
     }
 
-    private void getLastLocation() {
+
+    private void getWeather(String url, String api_key) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    longitude.setText(String.format("Longitude: %s", location.getLongitude()));
-                    latitude.setText(String.format("Latitude: %s", location.getLatitude()));
-                } else {
-                    longitude.setText("Nothing");
-                }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                builtUri = Uri.parse(url)
+                        .buildUpon()
+                        .appendQueryParameter(LAT_PARAM, String.valueOf(location.getLatitude()))
+                        .appendQueryParameter(LONG_PARAM, String.valueOf(location.getLongitude()))
+                        .appendQueryParameter(APIKEY_PARAM, api_key)
+                        .build();
+                apiKey.setText(builtUri.toString());
+            } else {
+                longitudeTextView.setText("Nothing");
             }
         });
     }
@@ -52,9 +66,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
-            } else {
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Location denied", Toast.LENGTH_SHORT).show();
             }
         }
